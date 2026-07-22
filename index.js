8e85cf3dd94bfaa3e6b636d86345a225afedddc9
Kendra Hartnett <kendrahartnett2208@gmail.com>
2026-07-22 15:19:01 -0500

    Adds fonts and styling for ui.

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ const habitForm = document.getElementById("habit-form");
 const habitInput = document.getElementById("habit-input");
 const habitContainer = document.getElementById("habit-container");
 
+// Icons
+
+const checkIcon = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+</svg>`;
+
+const trashIcon = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+</svg>`;
+
 //Add habit button click event listener
 
 addHabitButton.addEventListener("click", () => {
@@ -21,16 +31,27 @@ addHabitButton.addEventListener("click", () => {
     id: crypto.randomUUID(),
     name: habitInput.value,
     completed: false,
+    createdAt: new Date().toISOString(),
+
+    // Future enhancements for habit tracking could include additional properties such as:
+    // frequency: "daily", // or "weekly", "monthly"
+    // completedToday: false, // to track if the habit was completed today
+    // streak: 0, // to track the number of consecutive days the habit has been completed
+   
   };
   habitData.push(newHabit);
 
   const newHabitHTML = `
-    <div class="habit box" id="${newHabit.id}">
+    <div class="box" id="${newHabit.id}">
 
     <div class="w-full max-w-sm shadow-xl rounded-lg p-6 bg-white mt-6 ml-6">
+    <div class="mb-2">
    <span id="habitId-${newHabit.id}">${newHabit.name}</span>
-        <button id="complete-${newHabit.id}" class="complete-habit-button" onclick="onCompleteClick('${newHabit.id}')">Complete</button>
-        <button id="delete-${newHabit.id}" class="delete-habit-button" onclick="onDeleteClick('${newHabit.id}')">Delete</button>
+   </div>
+        <button id="complete-${newHabit.id}" class="complete-habit-button bg-green-400 text-white py-2 px-4 rounded hover:bg-green-500 transition duration-200" onclick="onCompleteClick('${newHabit.id}')">${checkIcon}</button>
+      
+      
+        <button id="delete-${newHabit.id}" class="delete-habit-button bg-green-400 text-white py-2 px-4 rounded hover:bg-green-500 transition duration-200" onclick="onDeleteClick('${newHabit.id}')">${trashIcon}</button>
     </div>
      
     </div>
